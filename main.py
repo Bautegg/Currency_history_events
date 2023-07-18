976a99df06fc531118e2578e2501d9454d7124b9
Bautegg <97198351+Bautegg@users.noreply.github.com>
2023-07-18 16:54:24 +0200

    plot added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 import plotly.express as px
+from dash import dcc
+
+location = 'currencies_data'
 
 
 def load_data(location):
@@ -14,15 +17,33 @@ def load_data(location):
     df = pd.concat(df_list, ignore_index=True)
     return df
 
+def data_cleansing():
+
+    df_cl = load_data(location)
+    df_date = pd.to_datetime(df_cl['<DATE>'], format="%Y%m%d")
+    df_year = df_date.dt.year
+    df_cl['Date'] = df_date
+    df_cl['Year'] = df_year
+
+    return df_cl
 
 def create_plot():
 
-    df_plot = load_data(location)
-    fig = px.line(df_plot, y='<OPEN>', x='<DATE>', color='<TICKER>')
+    df_plot = data_cleansing()
+    fig = px.line(df_plot, y='<OPEN>', x='Date', color='<TICKER>')
     fig.show()
 
 
+def create_checkbox():
+
+    df_check_list = load_data(location)
+    df_cl = df_check_list['<TICKER>'].unique()
+    dcc.Checklist(df_cl)
+    return df_cl
+
 if __name__ == '__main__':
-    location = 'currencies_data'
-    print(f'1 USD is X PLN: {load_data(location)} \n')
+
+    # print(f'LOADED_DATA: {load_data(location)} \n')
     print(f'PLOT: {create_plot()} \n')
+    print(f'Cleansed data: {data_cleansing()} \n')
+    print(f'CHECK_BOX: {create_checkbox()} \n\n')
